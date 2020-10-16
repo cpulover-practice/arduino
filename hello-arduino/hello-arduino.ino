7303563a7afc736101766692426e3421067b0fb4
Hieu Ngo <justacpulover@gmail.com>
2020-10-16 10:49:15 +0200

    Hello Arduino

---
+++ hello-arduino/hello-arduino.ino
@@ -0,0 +1,17 @@
+const int pinNumber = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // init the LED
+  pinMode(pinNumber, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(pinNumber, HIGH);
+  delay(1000);
+  digitalWrite(pinNumber, LOW);
+  delay(1000);
+}
