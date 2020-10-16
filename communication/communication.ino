cc91fac3748b2d24cbde3344b7157c6f6bcc8d59
Hieu Ngo <justacpulover@gmail.com>
2020-10-16 11:04:42 +0200

    Use Serial monitor to communicate with pc for debugging

---
+++ communication/communication.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  // begin communication with baud value in "Serial monitor"
+  Serial.begin(9600);
+  Serial.print("Hello computer from Arduino");  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
