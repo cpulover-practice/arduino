055d195dd73aaea074622cf6df1cb2e798dc5023
Hieu Ngo <justacpulover@gmail.com>
2020-10-18 00:38:13 +0200

    Button controls led (input vs output)

---
+++ button/button.ino
@@ -0,0 +1,26 @@
+#define BUTTON_PIN 7
+#define LED_PIN 13
+
+bool isOn = false;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonValue = digitalRead(BUTTON_PIN);
+  if (buttonValue == HIGH ) {
+    Serial.println("Button pressing!");
+    isOn = !isOn;
+    if (isOn) {
+      digitalWrite(LED_PIN, HIGH);
+    } else {
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+
+  delay(200);
+}
