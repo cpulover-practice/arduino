3a5b90b234569bb6975079a85db04bdbaab82520
Hieu Ngo <justacpulover@gmail.com>
2020-10-18 16:36:36 +0200

    Save data with EEPROM

---
+++ src/eeprom.cpp
@@ -0,0 +1,16 @@
+// #include <Arduino.h>
+// #include <EEPROM.h>
+
+// void setup()
+// {
+//     // EEPROM.write(3, 59);
+//     int value = EEPROM.read(3);
+
+//     Serial.begin(9600);
+//     Serial.print("Value in EEPROM[3] is: ");
+//     Serial.println(value);
+// }
+
+// void loop()
+// {
+// }
