163ecbf7bd8032ee3f8efc581e179fadbdc5a462
Hieu Ngo <justacpulover@gmail.com>
2020-10-18 13:17:22 +0200

    Read input from computer

---
+++ src/pc-input.cpp
@@ -0,0 +1,17 @@
+// #include <Arduino.h>
+
+// void setup()
+// {
+//     Serial.begin(9600);
+//     Serial.println("Start reading input...");
+// }
+
+// void loop()
+// {
+//     if (Serial.available() > 0)
+//     {
+//         int input = Serial.parseInt();
+//         Serial.print("Received data: ");
+//         Serial.println(input);
+//     }
+// }
